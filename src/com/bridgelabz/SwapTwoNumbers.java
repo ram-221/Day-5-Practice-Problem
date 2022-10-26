bbfb7214330dedb57983cb50938af0e784b95185
ram-221 <rameshmalli857.com>
2022-10-26 22:50:06 +0530

    Swap Two Numbers

---
+++ src/com/bridgelabz/SwapTwoNumbers.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+		
+
+		  int x, y, t; 
+	       Scanner sc = new Scanner(System.in);  
+	       System.out.println("Enter the value of X and Y");  
+	       x = sc.nextInt();  
+	       y = sc.nextInt();  
+	       System.out.println("before swapping numbers: "+x +"  "+ y);  
+	       //swapping  
+	       t = x;  
+	       x = y;  
+	       y = t;  
+	       System.out.println("After swapping: "+x +"   " + y);  
+	       System.out.println( );  
+	       sc.close();
+	}
+}
